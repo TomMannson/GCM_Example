4f78f48f48ef16b341710bcab042d626c43a5f14
TomMannson <tomasz.krol88@gmail.com>
2016-04-20 10:05:57 +0200

    C# code for push gcm

---
+++ c#/Notifications.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace WebDeploy.Utils
+{
+    public class Notifications
+    {
+        public static string SendNotification(string to, string message)
+        {
+            string SERVER_API_KEY = "AIzaSyBI-ad84UjYEBXK9-D33hmlrwnN7uVsC6M";
+            var SENDER_ID = "349243673588";
+            var value = message;
+            WebRequest tRequest;
+            tRequest = WebRequest.Create("https://android.googleapis.com/gcm/send");
+            tRequest.Method = "post";
+            tRequest.ContentType = " application/x-www-form-urlencoded;charset=UTF-8";
+            tRequest.Headers.Add(string.Format("Authorization: key={0}", SERVER_API_KEY));
+            tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
+            tRequest.ContentType = "application/json";
+
+            string topic = string.Format("/topics/{0}", to);
+            var json = JsonConvert.SerializeObject(new { to = topic, data = new { appId = message } });
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+            tRequest.ContentLength = byteArray.Length;
+
+            Stream dataStream = tRequest.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse tResponse = tRequest.GetResponse();
+
+            dataStream = tResponse.GetResponseStream();
+
+            StreamReader tReader = new StreamReader(dataStream);
+
+            string sResponseFromServer = tReader.ReadToEnd();
+
+
+            tReader.Close();
+            dataStream.Close();
+            tResponse.Close();
+            return sResponseFromServer;
+        }
+    }
+}
